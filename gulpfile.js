805230d0a6a40993ca94bbda6ca7c98cd6f11bd9
Alex Erickson <aelliotterickson@gmail.com>
2016-04-21 13:15:50 -0700

    remove build directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
+var sassGlob = require('gulp-sass-glob');
+var autoprefixer = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
 var browserify = require('browserify');
 var uglify = require('gulp-uglify');
@@ -10,6 +12,7 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var cleanCSS = require('gulp-clean-css');
 
 // JavaScript linting task
 gulp.task('jshint', function() {
@@ -21,14 +24,18 @@ gulp.task('jshint', function() {
 // Compile Sass task
 gulp.task('sass', function() {
   return gulp.src('site/scss/*.scss')
-    .pipe(sass())
+    .pipe(sassGlob())
+    .pipe(sass({
+      includePaths: require('node-neat').includePaths
+    }))
     .pipe(gulp.dest('site/css'));
 });
 
 // Watch task
 gulp.task('watch', function() {
-  gulp.watch('site/js/*.js', ['jshint']);
-  gulp.watch('site/scss/*.scss', ['sass']);
+  gulp.watch('site/js/**/*.js', ['jshint']);
+  gulp.watch('site/scss/**/*.scss', ['sass']);
+  gulp.watch('site/css/*.css', ['autoprefixer']);
 });
 
 // Minify index
@@ -51,6 +58,7 @@ gulp.task('scripts', function() {
 // Styles build task, concatenates all the files
 gulp.task('styles', function() {
   return gulp.src('site/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(concat('styles.css'))
     .pipe(gulp.dest('build/css'));
 });
@@ -62,8 +70,18 @@ gulp.task('images', function() {
     .pipe(gulp.dest('build/img'));
 });
 
+// Apply autoprefixer to scss
+gulp.task('autoprefixer', function () {
+	return gulp.src('site/css/*.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('site/css'));
+});
+
 // Default task
-gulp.task('default', ['jshint', 'sass', 'watch']);
+gulp.task('default', ['jshint', 'sass', 'autoprefixer', 'watch']);
 
 // Build task
 gulp.task('build', ['jshint', 'sass', 'html', 'scripts', 'styles', 'images']);
