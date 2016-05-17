a36e68c4701ba960a52361bf6a49c27bf60d2db5
Alex Erickson <aelliotterickson@gmail.com>
2016-05-17 10:47:20 -0700

    firepad makes it buggy

--- site/js/app.js
+++ site/js/app.js
@@ -1,35 +1,25 @@
-angular.module('Collaboratr', ['firebase'])
-.service('CollabService', ['$rootScope', function($rootScope){	
-	$rootScope.currentMode;
-	return {
-		newEditor: function() {
-			$rootScope.editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
-				lineNumbers: true,
-				mode: $rootScope.currentMode
-			});
-			$rootScope.editor.setOption("mode", $rootScope.currentMode);
-		}
-	}	
-}])
-.controller('CollabCtrl', ['$rootScope', '$scope', 'CollabService', '$firebaseObject', function($rootScope, $scope, CollabService, $firebaseObject){
+angular.module('Collaboratr', ['firebase', 'ui.codemirror'])
+.controller('CollabCtrl', ['$rootScope', '$scope', '$firebaseObject', function($rootScope, $scope, $firebaseObject){
 	var ref = new Firebase("https://collaboratr.firebaseio.com/data");
 
-	// $scope.data = $firebaseObject(ref);
+	$scope.data = $firebaseObject(ref);
 
-	// $scope.data.$bindTo($scope, "data").then(function(){
-	// 	console.log($scope.data.textarea);
-	// 	console.log($scope.data.users);
-	// });
-	$scope.data = {
-					'users': 'alex',
-					'textarea': '//your code goes here!'
-				};
+	$scope.data.$bindTo($scope, "data").then(function(){
+		ref.set({users: ['alex', 'rico', 'jean-jaques'],
+				 textarea: ''});
+	});
+	
 	$scope.modes = {'HTML': 'htmlmixed', 'CSS': 'css', 'JavaScript': 'javascript', 'PHP': 'php', 'Python': 'python', 'Ruby': 'ruby'};
 	$scope.currentMode = $rootScope.currentMode = $scope.modes.JavaScript;
-	$scope.editor = CollabService.newEditor();
+	$scope.editorOptions = {
+        lineWrapping : true,
+        lineNumbers: true,
+        mode: $rootScope.currentMode
+    };
+	
 	$scope.$watch('currentMode', function(value){
 		$rootScope.currentMode = value;
-		$rootScope.editor.setOption("mode", $rootScope.currentMode);
+		$scope.editorOptions.mode = $rootScope.currentMode;
 	}); 
-	console.log($scope.data.textarea);
+
 }]);
