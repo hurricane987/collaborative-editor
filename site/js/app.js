be774f0bc8c888c9ee9ab852c26b525593f46fdf
Alex Erickson <aelliotterickson@gmail.com>
2016-05-18 11:49:24 -0700

    meh

--- site/js/app.js
+++ site/js/app.js
@@ -2,11 +2,10 @@ angular.module('Collaboratr', ['firebase', 'ui.codemirror'])
 .controller('CollabCtrl', ['$rootScope', '$scope', '$firebaseObject', function($rootScope, $scope, $firebaseObject){
 	var ref = new Firebase("https://collaboratr.firebaseio.com/data");
 
-	$scope.data = $firebaseObject(ref);
+	obj = $firebaseObject(ref);
 
-	$scope.data.$bindTo($scope, "data").then(function(){
-		ref.set({users: ['alex', 'rico', 'jean-jaques'],
-				 textarea: ''});
+	obj.$bindTo($scope, "data").then(function(){
+		ref.set({textarea: '//your code goes here!'});
 	});
 	
 	$scope.modes = {'HTML': 'htmlmixed', 'CSS': 'css', 'JavaScript': 'javascript', 'PHP': 'php', 'Python': 'python', 'Ruby': 'ruby'};
@@ -16,10 +15,4 @@ angular.module('Collaboratr', ['firebase', 'ui.codemirror'])
         lineNumbers: true,
         mode: $rootScope.currentMode
     };
-	
-	$scope.$watch('currentMode', function(value){
-		$rootScope.currentMode = value;
-		$scope.editorOptions.mode = $rootScope.currentMode;
-	}); 
-
 }]);
